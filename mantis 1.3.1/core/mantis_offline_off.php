a8f105651ba099c4d6313a8d975819d6962cb38a
hajdus <32811798+hajdus@users.noreply.github.com>
2020-06-12 09:15:18 +0200

    Add files via upload

---
+++ mantis 1.3.1/core/mantis_offline_off.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html>
+  <head>
+    <title>MantisBT</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Refresh" content="600" />
+
+    <style type="text/css"> <!--
+      body {
+        background-color: white;
+      }
+
+      h1 {
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        font-weight: bold;
+        font-size: 20px;
+        text-align: center;
+        margin: 0;
+        padding: 5px;
+        color: black;
+        background-color: #c8c8e8;
+      }
+
+      p {
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        text-align: left;
+        margin: 10px;
+        padding: 0;
+      }
+
+      #box {
+        padding: 0;
+        margin-top: 20%;
+        margin-left: 25%;
+        margin-right: 25%;
+        border: 1px solid #777;
+        background-color: #eee;
+      }
+
+    --> </style>
+  </head>
+
+  <body>
+
+    <div id="box">
+
+      <h1>Mantis PSW</h1>
+
+      <p>
+      Przerwa techniczka. Serwis będzie dostępny po godzinie 15:15.
+		
+      </p>
+
+      <p>
+       
+      </p>
+
+    </div>
+
+  </body>
+</html>
